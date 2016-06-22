4f13b656f3cb6a5fa4c251c01e9e1e52da6dd471
Aakil Fernandes <aakilfernandes@gmail.com>
2016-06-22 16:37:49 -0400

    finish up unit testing and start of eip20

--- contracts/Shareit.sol
+++ contracts/Shareit.sol
@@ -10,12 +10,12 @@ contract Shareit{
 	struct Log {
 		bool isFinalized;
 		uint weiReceived;
+		uint weiCarried;
 		uint sharesInc;
 		uint shares;
 	}
 
 	struct Holder {
-		uint created;
 		uint shares;
 		uint weiUnpaid;
 		mapping(uint => HolderLog) logs;
@@ -26,6 +26,7 @@ contract Shareit{
 		uint sharesInc;
 		uint sharesDec;
 		uint shares;
+		uint weiRewarded;
 	}
 
 	function Shareit(uint _periodSeconds) {
@@ -45,10 +46,12 @@ contract Shareit{
 		if(logs[period].isFinalized)
 			throw;
 
-		if(period != getPeriodAt(created) && !logs[period - 1].isFinalized)
+		if(period != 0 && !logs[period - 1].isFinalized)
 			throw;
 
 		logs[period].shares = logs[period-1].shares + logs[period].sharesInc;
+		logs[period+1].weiCarried += (logs[period].weiReceived + logs[period].weiCarried) % logs[period].shares;
+
 		logs[period].isFinalized = true;
 
 	}
@@ -59,11 +62,12 @@ contract Shareit{
 			throw;
 
 		Holder holder = holders[addr];
+		Log log = logs[period];
 
 		if(holder.logs[period].isFinalized)
 			throw;
 
-		if(period != getPeriodAt(holder.created) && !holder.logs[period - 1].isFinalized)
+		if(period != 0 && !holder.logs[period - 1].isFinalized)
 			throw;
 
 		holder.logs[period].shares =
@@ -72,7 +76,12 @@ contract Shareit{
 			- holder.logs[period].sharesDec;
 
 		if (holder.logs[period].shares > 0) {
-			holder.weiUnpaid += ((holder.logs[period].shares * logs[period].weiReceived) / logs[period].shares);
+			holder.logs[period].weiRewarded =  (
+				(holder.logs[period].shares * (log.weiReceived + log.weiCarried - logs[period+1].weiCarried))
+				/
+				log.shares
+			);
+			holder.weiUnpaid += holder.logs[period].weiRewarded;
 		}
 
 		holder.logs[period].isFinalized = true;
@@ -101,42 +110,52 @@ contract Shareit{
 		logs[period].sharesInc += count;
 		holders[addr].shares += count;
 		holders[addr].logs[period].sharesInc += count;
-
-		if(holders[addr].created == 0){
-			holders[addr].created = now;
-		}
 	}
 
-	function depositWei() {
+	function() {
 		logs[getPeriod()].weiReceived += msg.value;
 	}
 
-	function withdrawWei() {
-		if(!msg.sender.send(holders[msg.sender].weiUnpaid))
+	function withdraw() {
+		uint weiUnpaid = holders[msg.sender].weiUnpaid;
+		holders[msg.sender].weiUnpaid = 0;
+		if(!msg.sender.call.value(weiUnpaid)())
+			throw;
+	}
+
+	function getHolderParams(address addr) constant returns(uint, uint) {
+		Holder holder = holders[addr];
+		return (holder.shares, holder.weiUnpaid);
+	}
+
+	function getLogParams(uint period) constant returns(bool, uint, uint, uint, uint) {
+		Log log = logs[period];
+		return (log.isFinalized, log.weiReceived, log.weiCarried, log.sharesInc, log.shares);
+	}
+
+	function getHolderLogParams(address addr, uint period) constant returns(bool, uint, uint, uint, uint) {
+		HolderLog holderLog = holders[addr].logs[period];
+		return (holderLog.isFinalized, holderLog.sharesInc, holderLog.sharesDec, holderLog.shares, holderLog.weiRewarded);
+	}
+
+	function totalSupply() constant returns (uint256 supply) {
+		return shares;
+	}
+
+	function balanceOf(address addr) constant returns (uint256 balance) {
+		return holders[addr].shares;
+	}
+
+	function transfer(address addr, uint256 count) {
+		if(holders[msg.sender].shares < count)
 			throw;
 
-		holders[msg.sender].weiUnpaid = 0;
-	}
+		uint period = getPeriod();
 
-	function getHolderParams(address addr) constant returns(uint, uint, uint) {
-		Holder holder = holders[addr];
-		return (holder.created, holder.shares, holder.weiUnpaid);
-	}
-
-	function getLogParams(uint period) constant returns(bool, uint, uint, uint) {
-		Log log = logs[period];
-		return (log.isFinalized, log.weiReceived, log.sharesInc, log.shares);
-	}
-
-	function getHolderLogParams(address addr, uint period) constant returns(bool, uint, uint, uint) {
-		HolderLog holderLog = holders[addr].logs[period];
-		return (holderLog.isFinalized, holderLog.sharesInc, holderLog.sharesDec, holderLog.shares);
-	}
-
-	function bootstrapHolder(address addr) {
-		if(holders[addr].created == 0){
-			holders[addr].created = getPeriod();
-		}
+		holders[msg.sender].shares -= count;
+		holders[msg.sender].logs[period].sharesDec += count;
+		holders[addr].shares += count;
+		holders[addr].logs[period].sharesInc += count;
 	}
 
 }
--- test/Shareit.js
+++ test/Shareit.js
@@ -7,6 +7,7 @@ const web3 = chaithereum.web3
 const chai = chaithereum.chai
 const expect = chaithereum.chai.expect
 
+let increasedTime = 0
 const periodSeconds = 60 * 60 * 24 * 7
 let shareit
 let created
@@ -54,7 +55,7 @@ describe('Shareit', () => {
 	})
 
 	it('should correctly getPeriod()', () => {
-		return shareit.getPeriod.q().should.eventually.be.bignumber.equal(getPeriod())
+		return shareit.getPeriod.q().should.eventually.be.bignumber.equal(0)
 	})
 
 	it('should correctly getPeriodAt(0)', () => {
@@ -74,97 +75,98 @@ describe('Shareit', () => {
 	})
 
 	it('should not be able to inflate from account1', () => {
-		return shareit.inflate.q(accounts[1], 10, { from: accounts[1] }).should.be.rejected
+		return shareit.inflate.q(accounts[0], 10, { from: accounts[1] }).should.be.rejected
 	})
 
-	it('should be able to inflate from account', () => {
-		return shareit.inflate.q(accounts[0], 10).should.be.fulfilled
+	it('should be able to inflate accounts[0] from account', () => {
+		return shareit.inflate.q(accounts[0], 5).should.be.fulfilled
+	})
+
+	it('should be able to inflate accounts[1] from account', () => {
+		return shareit.inflate.q(accounts[1], 5).should.be.fulfilled
 	})
 
 	it('should have 10 shares', () => {
 		return shareit.shares.q().should.eventually.be.bignumber.equal(10)
 	})
 
-	it('should be able to deposit 10 wei', () => {
-		return shareit.depositWei.q({ value: 10 }).should.be.fulfilled
+	it('should be able to send shareit 12 wei', () => {
+		return web3.eth.sendTransaction.q({ to: shareit.address, value: 12 }).should.be.fulfilled
 	})
 
 	it('should not be able to finalize log 0', () => {
 		return shareit.finalizeLog.q(0).should.be.rejected
 	})
 
-	it('should not be able to finalize holder log 0', () => {
-		return shareit.finalizeHolderLog.q(0).should.be.rejected
+	it('should not be able to finalize account0 log 0', () => {
+		return shareit.finalizeHolderLog.q(accounts[0], 0).should.be.rejected
 	})
 
 	describe('log 0', () => {
 
-		let log0Params
+		let logParams
 
 		it('should be retreivable', () => {
-			return shareit.getLogParams.q(0).then((logParams) => {
-				log0Params = logParams
+			return shareit.getLogParams.q(0).then((_logParams) => {
+				logParams = _logParams
 			})
 		})
 
 		it('should not be finalized', () => {
-			expect(log0Params[0]).to.be.false
+			expect(logParams[0]).to.be.false
 		})
 
-		it('should have received 10 wei', () => {
-			expect(log0Params[1]).to.be.bignumber.equal(10)
+		it('should have received 12 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(12)
+		})
+
+		it('should have carried 0 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(0)
 		})
 
 		it('should have increased shares by 10', () => {
-			expect(log0Params[2]).to.be.bignumber.equal(10)
+			expect(logParams[3]).to.be.bignumber.equal(10)
 		})
 
 		it('should have total shares of 0, since it hasnt been finalized yet', () => {
-			expect(log0Params[3]).to.be.bignumber.equal(0)
+			expect(logParams[4]).to.be.bignumber.equal(0)
 		})
 
 	})
 
-	// describe('holder', () => {
-	// 	let holder
+	describe('account0 log 0', () => {
 
-	// 	it('should be retreivable', () => {
-	// 		return share.getHolder.q(account,)
-	// 	})
-	// })
-
-	describe('holder log 0', () => {
-
-		let holderLog0Params
+		let holderLogParams
 
 		it('should be retreivable', () => {
-			return shareit.getHolderLogParams.q(account, 0).then((holderLogParams) => {
-				holderLog0Params = holderLogParams
+			return shareit.getHolderLogParams.q(accounts[0], 0).then((_holderLogParams) => {
+				holderLogParams = _holderLogParams
 			})
 		})
 
 		it('should not be finalized', () => {
-			expect(holderLog0Params[0]).to.be.false
+			expect(holderLogParams[0]).to.be.false
 		})
 
-		it('should have increased shares by 10', () => {
-			expect(holderLog0Params[1]).to.be.bignumber.equal(10)
+		it('should have increased shares by 5', () => {
+			expect(holderLogParams[1]).to.be.bignumber.equal(5)
 		})
 
 		it('should have decreased shares by 0', () => {
-			expect(holderLog0Params[2]).to.be.bignumber.equal(0)
+			expect(holderLogParams[2]).to.be.bignumber.equal(0)
 		})
 
 		it('should have total shares of 0, since it hasnt been finalized yet', () => {
-			expect(holderLog0Params[3]).to.be.bignumber.equal(0)
+			expect(holderLogParams[3]).to.be.bignumber.equal(0)
 		})
 
 	})
 
 })
 
-describe('jump', () => {
+describe('period 1', () => {
 	it(`should jump by ${periodSeconds} seconds`, (done) => {
+		increasedTime += periodSeconds
 		web3.eth.getBlock.q('latest').then((blockBeforeJump) => {
 			chaithereum.provider.manager.increaseTime(periodSeconds)
 			web3.eth.getBlock.q('latest').then((blockAfterJump) => {
@@ -179,10 +181,468 @@ describe('jump', () => {
 		}, (err) => { done(err) })
 
 	})
+
+	it('should correctly getPeriod()', () => {
+		return shareit.getPeriod.q().should.eventually.be.bignumber.equal(1)
+	})
+
+	it('should correctly getPeriodAt(0)', () => {
+		return shareit.getPeriodAt.q(0).should.eventually.be.bignumber.equal(0)
+	})
+
+	it('should correctly getPeriodAt(randomTime) x 10', () => {
+		return web3.Q.all(Object.keys(new Int8Array(10)).map(() => {
+			const randomTime = getRandomTime()
+			const period = getPeriod(randomTime)
+			return shareit.getPeriodAt.q(randomTime).should.eventually.bignumber.equal(period)
+		}))
+	})
+
+	it('should not be able to finalize account0 log 0, since log 0 has not been finalized', () => {
+		return shareit.finalizeHolderLog.q(accounts[0], 0).should.be.rejected
+	})
+
+	it('should be able to finalize log 0', () => {
+		return shareit.finalizeLog.q(0).should.be.fulfilled
+	})
+
+	it('should not be able to re-finalize log 0', () => {
+		return shareit.finalizeLog.q(0).should.be.rejected
+	})
+
+	it('should be able to finalize account0 log 0', () => {
+		return shareit.finalizeHolderLog.q(accounts[0], 0).should.be.fulfilled
+	})
+
+	it('should not be able to re-finalize account0 log 0', () => {
+		return shareit.finalizeHolderLog.q(accounts[0], 0).should.be.rejected
+	})
+
+	it('should be able to send shareit 15 wei', () => {
+		return web3.eth.sendTransaction.q({ to: shareit.address, value: 15 }).should.be.fulfilled
+	})
+
+	describe('log 0', () => {
+
+		let logParams
+
+		it('should be retreivable', () => {
+			return shareit.getLogParams.q(0).then((_logParams) => {
+				logParams = _logParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(logParams[0]).to.be.true
+		})
+
+		it('should have received 12 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(12)
+		})
+
+		it('should have carried 0 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(0)
+		})
+
+		it('should have increased shares by 10', () => {
+			expect(logParams[3]).to.be.bignumber.equal(10)
+		})
+
+		it('should have total shares of 10', () => {
+			expect(logParams[4]).to.be.bignumber.equal(10)
+		})
+
+	})
+
+	describe('log 1', () => {
+
+		let logParams
+
+		it('should be retreivable', () => {
+			return shareit.getLogParams.q(1).then((_logParams) => {
+				logParams = _logParams
+			})
+		})
+
+		it('should not be finalized', () => {
+			expect(logParams[0]).to.be.false
+		})
+
+		it('should have received 15 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(15)
+		})
+
+		it('should have carried 2 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(2)
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(logParams[3]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 0, since it hasnt been finalized yet', () => {
+			expect(logParams[4]).to.be.bignumber.equal(0)
+		})
+
+	})
+
+	describe('account0 log 0', () => {
+
+		let holderLogParams
+
+		it('should be retreivable', () => {
+			return shareit.getHolderLogParams.q(accounts[0], 0).then((_holderLogParams) => {
+				holderLogParams = _holderLogParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(holderLogParams[0]).to.be.true
+		})
+
+		it('should have increased shares by 5', () => {
+			expect(holderLogParams[1]).to.be.bignumber.equal(5)
+		})
+
+		it('should have decreased shares by 0', () => {
+			expect(holderLogParams[2]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 5', () => {
+			expect(holderLogParams[3]).to.be.bignumber.equal(5)
+		})
+
+		it('should have rewarded wei of 5', () => {
+			expect(holderLogParams[4]).to.be.bignumber.equal(5)
+		})
+
+	})
+
+	describe('holder', () => {
+		let holderParams
+
+		it('should be retreivable', () => {
+			return shareit.getHolderParams.q(account).then((_holderParams) => {
+				holderParams = _holderParams
+			})
+		})
+
+		it('should have 5 shares', () => {
+			expect(holderParams[0]).to.be.bignumber.equal(5)
+		})
+
+		it('should have 5 wei unpaid', () => {
+			expect(holderParams[1]).to.be.bignumber.equal(5)
+		})
+	})
+
+	describe('transfers', () => {
+
+		it('should not be able to transfer 6 shares from account0 to account2', () => {
+			return shareit.transfer.q(accounts[2], 6).should.be.rejected
+		})
+
+		it('should transfer 1 shares from account0 to account3', () => {
+			return shareit.transfer.q(accounts[3], 1).should.be.fulfilled
+		})
+
+		it('should transfer 2 shares from account1 to account2', () => {
+			return shareit.transfer.q(accounts[2], 2, { from: accounts[1] }).should.be.fulfilled
+		})
+
+		it('accounts[0] should have 4 shares', () => {
+			return shareit.balanceOf.q(accounts[0]).should.eventually.be.bignumber.equal(4)
+		})
+
+		it('accounts[1] should have 3 shares', () => {
+			return shareit.balanceOf.q(accounts[1]).should.eventually.be.bignumber.equal(3)
+		})
+
+		it('accounts[2] should have 2 shares', () => {
+			return shareit.balanceOf.q(accounts[2]).should.eventually.be.bignumber.equal(2)
+		})
+
+		it('accounts[3] should have 1 shares', () => {
+			return shareit.balanceOf.q(accounts[3]).should.eventually.be.bignumber.equal(1)
+		})
+
+	})
+})
+
+describe('period 2', () => {
+	it(`should jump by ${periodSeconds} seconds`, (done) => {
+		increasedTime += periodSeconds
+		web3.eth.getBlock.q('latest').then((blockBeforeJump) => {
+			chaithereum.provider.manager.increaseTime(periodSeconds)
+			web3.eth.getBlock.q('latest').then((blockAfterJump) => {
+				const timeJumped = blockAfterJump.timestamp - blockBeforeJump.timestamp
+				try {
+					expect(timeJumped).to.be.within(periodSeconds, periodSeconds + 5)
+				} catch (err) {
+					return done(err)
+				}
+				done()
+			}, (err) => { done(err) })
+		}, (err) => { done(err) })
+
+	})
+
+	it('should be able to send shareit 2 wei', () => {
+		return web3.eth.sendTransaction.q({ to: shareit.address, value: 2 }).should.be.fulfilled
+	})
+})
+
+describe('period 3', () => {
+	it(`should jump by ${periodSeconds} seconds`, (done) => {
+		increasedTime += periodSeconds
+		web3.eth.getBlock.q('latest').then((blockBeforeJump) => {
+			chaithereum.provider.manager.increaseTime(periodSeconds)
+			web3.eth.getBlock.q('latest').then((blockAfterJump) => {
+				const timeJumped = blockAfterJump.timestamp - blockBeforeJump.timestamp
+				try {
+					expect(timeJumped).to.be.within(periodSeconds, periodSeconds + 5)
+				} catch (err) {
+					return done(err)
+				}
+				done()
+			}, (err) => { done(err) })
+		}, (err) => { done(err) })
+
+	})
+
+
+	it('should not be able to finalize log 2, since period 1 has not been finalized', () => {
+		return shareit.finalizeLog.q(2).should.be.rejected
+	})
+
+	it('should be able to finalize log 1 and 2', () => {
+		return web3.Q.all([
+			shareit.finalizeLog.q(1).should.be.fulfilled,
+			shareit.finalizeLog.q(2).should.be.fulfilled
+		])
+	})
+
+	it('should not be able to finalize account0 log 2, since account0 log 1 has not been finalized', () => {
+		return shareit.finalizeHolderLog.q(accounts[0], 2).should.be.rejected
+	})
+
+	it('should be able to finalize account0 log 1 and 2', () => {
+		return web3.Q.all([
+			shareit.finalizeHolderLog.q(accounts[0], 1).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[0], 2).should.be.fulfilled
+		])
+	})
+	
+	it('should be able to finalize account1 log 0, 1 and 2', () => {
+		return web3.Q.all([
+			shareit.finalizeHolderLog.q(accounts[1], 0).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[1], 1).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[1], 2).should.be.fulfilled
+		])
+	})
+
+	it('should be able to finalize account2 log 0, 1 and 2', () => {
+		return web3.Q.all([
+			shareit.finalizeHolderLog.q(accounts[2], 0).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[2], 1).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[2], 2).should.be.fulfilled
+		])
+	})
+
+	it('should be able to finalize account3 log 0, 1 and 2', () => {
+		return web3.Q.all([
+			shareit.finalizeHolderLog.q(accounts[3], 0).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[3], 1).should.be.fulfilled,
+			shareit.finalizeHolderLog.q(accounts[3], 2).should.be.fulfilled
+		])
+	})
+
+	describe('log 1', () => {
+
+		let logParams
+
+		it('should be retreivable', () => {
+			return shareit.getLogParams.q(1).then((_logParams) => {
+				logParams = _logParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(logParams[0]).to.be.true
+		})
+
+		it('should have received 15 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(15)
+		})
+
+		it('should have carried 2 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(2)
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(logParams[3]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 10', () => {
+			expect(logParams[4]).to.be.bignumber.equal(10)
+		})
+
+	})
+
+	describe('log 2', () => {
+
+		let logParams
+
+		it('should be retreivable', () => {
+			return shareit.getLogParams.q(2).then((_logParams) => {
+				logParams = _logParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(logParams[0]).to.be.true
+		})
+
+		it('should have received 2 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(2)
+		})
+
+		it('should have carried 7 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(7)
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(logParams[3]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 10', () => {
+			expect(logParams[4]).to.be.bignumber.equal(10)
+		})
+
+	})
+
+	describe('log 3', () => {
+
+		let logParams
+
+		it('should be retreivable', () => {
+			return shareit.getLogParams.q(3).then((_logParams) => {
+				logParams = _logParams
+			})
+		})
+
+		it('should not be finalized', () => {
+			expect(logParams[0]).to.be.false
+		})
+
+		it('should have received 0 wei', () => {
+			expect(logParams[1]).to.be.bignumber.equal(0)
+		})
+
+		it('should have carried 9 wei', () => {
+			expect(logParams[2]).to.be.bignumber.equal(9)
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(logParams[3]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 0, since it hasnt been finalized yet', () => {
+			expect(logParams[4]).to.be.bignumber.equal(0)
+		})
+
+	})
+
+
+
+	describe('account0 log 1', () => {
+
+		let holderLogParams
+
+		it('should be retreivable', () => {
+			return shareit.getHolderLogParams.q(accounts[0], 1).then((_holderLogParams) => {
+				holderLogParams = _holderLogParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(holderLogParams[0]).to.be.true
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(holderLogParams[1]).to.be.bignumber.equal(0)
+		})
+
+		it('should have decreased shares by 1', () => {
+			expect(holderLogParams[2]).to.be.bignumber.equal(1)
+		})
+
+		it('should have total shares of 4', () => {
+			expect(holderLogParams[3]).to.be.bignumber.equal(4)
+		})
+
+		it('should have rewarded wei of 4', () => {
+			expect(holderLogParams[4]).to.be.bignumber.equal(4)
+		})
+
+	})
+
+	describe('account0 log 2', () => {
+
+		let holderLogParams
+
+		it('should be retreivable', () => {
+			return shareit.getHolderLogParams.q(accounts[0], 2).then((_holderLogParams) => {
+				holderLogParams = _holderLogParams
+			})
+		})
+
+		it('should be finalized', () => {
+			expect(holderLogParams[0]).to.be.true
+		})
+
+		it('should have increased shares by 0', () => {
+			expect(holderLogParams[1]).to.be.bignumber.equal(0)
+		})
+
+		it('should have decreased shares by 0', () => {
+			expect(holderLogParams[2]).to.be.bignumber.equal(0)
+		})
+
+		it('should have total shares of 4', () => {
+			expect(holderLogParams[3]).to.be.bignumber.equal(4)
+		})
+
+		it('should have rewarded wei of 0', () => {
+			expect(holderLogParams[4]).to.be.bignumber.equal(0)
+		})
+
+	})
+
+	describe('account0 holder params', () => {
+		let holderParams
+
+		it('should be retreivable', () => {
+			return shareit.getHolderParams.q(account).then((_holderParams) => {
+				holderParams = _holderParams
+			})
+		})
+
+		it('should have 4 shares', () => {
+			expect(holderParams[0]).to.be.bignumber.equal(4)
+		})
+
+		it('should have 9 wei unpaid', () => {
+			expect(holderParams[1]).to.be.bignumber.equal(9)
+		})
+	})
+
+
+	
 })
 
 function getNow(){
-	return Math.floor((new Date).getTime() / 1000)
+	return increasedTime + Math.floor((new Date).getTime() / 1000)
 }
 
 function getPeriod(time){
